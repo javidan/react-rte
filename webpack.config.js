711943b0624463be2d0f72f085b6ca1a34509360
Javidan Guliyev <q.cavidan@gmail.com>
2017-06-29 01:20:52 +0400

    added dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 /*eslint-env node */
 var path = require('path');
+var webpack = require("webpack")
 
 var loaders = [
   {
@@ -28,4 +29,32 @@ module.exports = [{
     filename: '[name].js',
   },
   module: {loaders: loaders},
+},
+{
+  entry: './src/RichTextEditor.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'react-rte.js',
+    libraryTarget: 'commonjs2',
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+  module: {loaders: loaders},
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: true,
+      comments: true,
+      mangle: false,
+      compress: {
+        dead_code: true,
+      },
+    }),
+  ],
 }];
